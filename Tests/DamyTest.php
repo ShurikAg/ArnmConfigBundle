7da6c54f1440d71252cc26ca95cd565b5970ce2f
Alex Agulyansky <alex@iibspro.com>
2012-10-03 00:02:02 -0700

    Adding damy test to get the build passing

---
+++ Tests/DamyTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace Arnm\ConfigBundle\Tests;
+
+/**
+ * Damy test case.
+ */
+class DamyTest extends \PHPUnit_Framework_TestCase
+{
+    public function testDamy()
+    {
+        $this->assertTrue(true);
+    }
+}
+
