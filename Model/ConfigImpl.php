8658a5dd4fe2fe826033d33e727f03123c35c4f3
Alex Agulyansky <alex@iibspro.com>
2012-11-03 02:24:31 -0700

    Created default implementation of field list for config model.

--- Model/ConfigImpl.php
+++ Model/ConfigImpl.php
@@ -9,4 +9,13 @@ use Arnm\ConfigBundle\Model\ConfigInterface;
  */
 abstract class ConfigImpl implements ConfigInterface
 {
+
+    /**
+     * {@inheritdoc}
+     * @see Arnm\ConfigBundle\Model.ConfigInterface::getFieldsList()
+     */
+    public function getFieldsList()
+    {
+        return array_keys(get_object_vars($this));
+    }
 }
