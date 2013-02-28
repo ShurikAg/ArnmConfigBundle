20c8bd51f56dcd031ec6c54c6ed43ea10be93fc0
Alex Agulyansky <alex@iibspro.com>
2013-02-28 04:38:41 -0800

    Added bundle info as a first implementation and a first attempt to use the automatic registration through compiler pass.

---
+++ ConfigBundleInfo.php
@@ -0,0 +1,14 @@
+<?php
+namespace Arnm\ConfigBundle;
+
+use Arnm\CoreBundle\Bundle\BundleInfoInterface;
+/**
+ * This is bundles info class
+ *
+ * @author Alex Agulyansky <alex@iibspro.com>
+ * @copyright Copyright (c) 2013, IIB Solutions Ltd.
+ */
+class ConfigBundleInfo implements BundleInfoInterface
+{
+
+}
